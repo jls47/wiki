5e65b50beaf0a328e0568e9f33c4f254cedfb85c
John Luke Schaefer <lukeschaefer@gmail.com>
2017-07-25 23:55:30 -0700

    static files are operational!

--- wiki/settings.py
+++ wiki/settings.py
@@ -121,8 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'wiki/staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'))
+STATICFILES_DIRS = ('static',)
