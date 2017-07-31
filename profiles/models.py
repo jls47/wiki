b59be9e8fe813b15950fb82e3e0d41ca374f88c0
John Luke Schaefer <lukeschaefer@gmail.com>
2017-07-30 18:16:53 -0700

    newstuff

--- profiles/models.py
+++ profiles/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-
+from ckeditor.fields import RichTextField
 # Create your models here.
 
 class Profile(models.Model):
-    summary = models.TextField(max_length=600)
+    summary = RichTextField(max_length=600)
     edited = models.CharField(max_length=600)
     created = models.CharField(max_length=600)
 
